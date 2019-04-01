462ad380e7d3be4caf2d686ad711a968b3aa5fd5
Jiang Chenhao <jch001@sjtu.edu.cn>
2019-04-01 20:30:17 +0800

    Update 7_reverse-integer.py

--- 1-50/7_reverse-integer.py
+++ 1-50/7_reverse-integer.py
@@ -1,6 +1,7 @@
 """
 Given a 32-bit signed integer, reverse digits of an integer.
-Note:Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+Note:Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1].
+For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 Ex_1:
 input:123
 output:321
